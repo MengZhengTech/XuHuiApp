29897160f70f1ae0e42e0533970b9873119a487e
qinghuai.huang <837084459@qq.com>
2017-11-03 11:49:39 +0800

    commit test

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native!11
           <Ionicons name='ios-search' size={20}/>
         </Text>
         <Text style={styles.instructions}>
