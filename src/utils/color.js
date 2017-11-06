653616c8f1ddf38b8839799a336644752c3ea725
z <z>
2017-11-06 14:40:49 +0800

    添加工具类

---
+++ src/utils/color.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Huai on 17/11/6.
+ */
+
+'use strict';
+
+export default {
+    mainColor: '#DB232E',
+    bottomTabBarBgColor: '#d33e39',   //底部切换栏背景颜色
+    activityColor: '#DB232E',   //文字或图标选中颜色
+    inactiveColor: '#999',   //文字及图标默认颜色
+    titelBarColor: '#CDCDB4',   //标题栏颜色
+}
--- src/utils/screen.js
+++ src/utils/screen.js
@@ -10,7 +10,8 @@ import {
 export default {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-    bottomTabHeight: 60,
-    headerHeight: 50,
+    bottomTabHeight: 60, //底部导航栏高度
+    headerHeight: 50, // 标题栏高度
+    tabIconSize: 25, //底部tab图标大小
     statesBarHeight: (Platform.OS === 'ios'? 20: 0),
 }
